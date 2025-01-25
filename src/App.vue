97eeab2526063f7f314ef408f582b8db42895a4c
Marcos <marcosjfr3@gmail.com>
2025-01-25 18:45:48 -0400

    work in progres for API

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
   import GetLocation from './components/GetLocation.vue';
+  import WeatherReport from './components/WeatherReport.vue';
 </script>
 
 
 <template>
   <GetLocation/>
+  <WeatherReport/>
 </template>
 
 
